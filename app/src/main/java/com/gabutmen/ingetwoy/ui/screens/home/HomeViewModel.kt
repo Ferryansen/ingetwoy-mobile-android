03b629f00fa33203982b030b7483cb53099c8a40
Ferryansen <kwanan57@gmail.com>
2026-06-26 08:43:01 +0700

    added connection between homeviewmodel and productrepo

--- app/src/main/java/com/gabutmen/ingetwoy/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/gabutmen/ingetwoy/ui/screens/home/HomeViewModel.kt
@@ -1,57 +1,18 @@
 package com.gabutmen.ingetwoy.ui.screens.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.gabutmen.ingetwoy.data.model.Product
-import com.gabutmen.ingetwoy.data.model.ReminderOffset
-import com.gabutmen.ingetwoy.data.model.ReminderUnit
-import kotlinx.coroutines.flow.MutableStateFlow
+import com.gabutmen.ingetwoy.data.repository.ProductRepository
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import java.time.LocalDate
+import kotlinx.coroutines.flow.stateIn
 
-class HomeViewModel: ViewModel() {
-    private val _items = MutableStateFlow<List<Product>>(
-        listOf(
-            Product(
-                id = 1L,
-                name = "iPhone 17",
-                category = "Gadget",
-                purchaseDate = LocalDate.of(2026,6,12),
-                expirationDate = LocalDate.of(2027,6,12),
-                notes = null,
-                extraReminders = listOf(
-                    ReminderOffset(
-                        14,
-                        ReminderUnit.DAY
-                    )
-                )
-            ),
-            Product(
-                id = 2L,
-                name = "Samsung TV 32 Inch",
-                category = "Home Appliance",
-                purchaseDate = LocalDate.of(2026,4,21),
-                expirationDate = LocalDate.of(2028,4,11),
-                notes = "Check any dots problems",
-                extraReminders = listOf(
-                    ReminderOffset(
-                        1,
-                        ReminderUnit.YEAR
-                    ),
-                    ReminderOffset(
-                        1,
-                        ReminderUnit.MONTH
-                    ),
-                    ReminderOffset(
-                        14,
-                        ReminderUnit.DAY
-                    ),
-                    ReminderOffset(
-                        7,
-                        ReminderUnit.DAY
-                    )
-                )
-            )
-        )
+class HomeViewModel(private val productRepository: ProductRepository): ViewModel() {
+    private val _items = productRepository.getAllProducts().stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = emptyList()
     )
     val items: StateFlow<List<Product>> = _items
 }

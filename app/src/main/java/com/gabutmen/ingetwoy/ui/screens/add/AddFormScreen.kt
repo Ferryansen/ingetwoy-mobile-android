71d4262bed092aa088ee341897cb950ae85dc357
Ferryansen <kwanan57@gmail.com>
2026-07-29 00:01:22 +0700

    added another form fields

--- app/src/main/java/com/gabutmen/ingetwoy/ui/screens/add/AddFormScreen.kt
+++ app/src/main/java/com/gabutmen/ingetwoy/ui/screens/add/AddFormScreen.kt
@@ -1,22 +1,29 @@
 package com.gabutmen.ingetwoy.ui.screens.add
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableLongStateOf
@@ -29,14 +36,33 @@ import androidx.compose.ui.input.key.type
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.gabutmen.ingetwoy.navigation.Routes
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+enum class DateField {
+    PURCHASE,
+    EXPIRE
+}
+
+private fun dateConverter(millis: Long?): String {
+    if(millis == null) return "Select date"
+    val date = Instant.ofEpochMilli(millis).atZone(ZoneId.systemDefault()).toLocalDate()
+
+    return date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddFormScreen(navController: NavHostController) {
     var productName by rememberSaveable { mutableStateOf("") }
     var notes by rememberSaveable { mutableStateOf("") }
+
     var purchaseDate by rememberSaveable { mutableStateOf<Long?>(null) }
+    val purchaseDatePickerState = rememberDatePickerState()
     var expirationDate by rememberSaveable { mutableStateOf<Long?>(null) }
+    val expirationDatePickerState = rememberDatePickerState()
+    var currPicker: DateField? by remember { mutableStateOf(null) }
 
     val categories = listOf("Gadget", "Home Appliances", "Others")
     var selectedCategory by rememberSaveable { mutableStateOf(categories[0]) }
@@ -110,7 +136,61 @@ fun AddFormScreen(navController: NavHostController) {
                 }
             }
 
+            Box(
+                modifier = Modifier.clickable { currPicker = DateField.PURCHASE }
+            ) {
+                TextField(
+                    value = dateConverter(purchaseDate),
+                    onValueChange = {},
+                    readOnly = true,
+                    enabled = false,
+                    label = {Text("Purchase Date")},
+                )
+            }
+
+            Box(
+                modifier = Modifier.clickable { currPicker = DateField.EXPIRE }
+            ) {
+                TextField(
+                    value = dateConverter(expirationDate),
+                    onValueChange = {},
+                    readOnly = true,
+                    enabled = false,
+                    label = {Text("Expiration Date")}
+                )
+            }
+
+
             Text("Reminder related field will be updated shortly")
+        }
+    }
+
+    if(currPicker != null) {
+        DatePickerDialog(
+            onDismissRequest = { currPicker = null },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        if(currPicker == DateField.PURCHASE) {
+                            purchaseDate = purchaseDatePickerState.selectedDateMillis
+                        } else if(currPicker == DateField.EXPIRE) {
+                            expirationDate = expirationDatePickerState.selectedDateMillis
+                        }
+
+                        currPicker = null
+                    }
+                ) {
+                    Text("Ok!")
+                }
+            }
+        ) {
+            DatePicker(
+                state = if (currPicker == DateField.PURCHASE) {
+                    purchaseDatePickerState
+                } else {
+                    expirationDatePickerState
+                }
+            )
         }
     }
 }

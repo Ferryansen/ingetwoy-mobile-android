54c144fb83ca01a875441e8c2d22c1331e7cc884
Ferryansen <kwanan57@gmail.com>
2026-08-12 23:04:27 +0700

    updated workmanager registration

--- app/src/main/java/com/gabutmen/ingetwoy/IngetWoyApp.kt
+++ app/src/main/java/com/gabutmen/ingetwoy/IngetWoyApp.kt
@@ -1,9 +1,10 @@
 package com.gabutmen.ingetwoy
 
 import android.app.Application
+import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequest
 import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkRequest
+import androidx.work.WorkManager
 import com.gabutmen.ingetwoy.worker.ReminderWorker
 import dagger.hilt.android.HiltAndroidApp
 import java.util.concurrent.TimeUnit
@@ -13,8 +14,14 @@ class IngetWoyApp: Application() {
     override fun onCreate() {
         super.onCreate()
 
-        val testWorkRequest: WorkRequest = PeriodicWorkRequestBuilder<ReminderWorker>(
+        val reminderWorkRequest: PeriodicWorkRequest = PeriodicWorkRequestBuilder<ReminderWorker>(
             24, TimeUnit.HOURS
         ).build()
+
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+            "reminder_check_work",
+            ExistingPeriodicWorkPolicy.KEEP,
+            reminderWorkRequest
+        )
     }
 }
